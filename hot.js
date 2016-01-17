3dee40d2ca70b990eda6627f6a34319a3ddd5b9f
kasselTrankos <alvaro.touzon@gmail.com>
2016-01-17 02:02:42 +0100

    remove info in hot from webpack

--- hot.js
+++ hot.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var http = require('http');
+var config = require('./twitter/config');
 var webpackConfig = require('./webpack.hot');
 var express = require('express');
 var app  = express();
@@ -13,14 +14,21 @@ var processPath = process.env.PROCESS_PATH || '/public';
   var webpack = require('webpack');
   var compiler = webpack(webpackConfig);
 
-  app.use(require("webpack-dev-middleware")(compiler, {
-    noInfo: false, publicPath: "/", stats: { colors: true }
-  }));
+app.use(require("webpack-dev-middleware")(compiler, {
+    noInfo: true, publicPath: "/", stats: { colors: true }
+}));
+app.use(require("webpack-hot-middleware")(compiler, {
+  log: console.log, path: '/__webpack_hmr', noInfo: true, heartbeat: 10 * 1000
+}));
+})();// Proxy to TWITTER server
+app.use('/' + config.services.twitter.path, (req, res) => {
+  proxyTwitter.web(req, res);
+});
 
-  app.use(require("webpack-hot-middleware")(compiler, {
-    log: console.log, path: '/__webpack_hmr', heartbeat: 10 * 1000
-  }));
-})();
+// Proxy to TWITTER server
+app.use('/' + config.services.twitter.path, (req, res) => {
+  proxyTwitter.web(req, res);
+});
 
 
 app.get('/', function (req, res) {
